1d94693a55dda4d046fdcd6dda0933802f967c35
29th-Day <43274844+29th-Day@users.noreply.github.com>
2025-05-16 19:22:59 +0200

    remove panic handler

--- src/lib.rs
+++ src/lib.rs
@@ -17,28 +17,3 @@ pub mod storage;
 pub mod prelude {
     pub use crate::config::{Attachable, ConfigMenu};
 }
-
-#[cfg(feature = "panic_handler")]
-#[panic_handler]
-fn panic_handler(info: &core::panic::PanicInfo) -> ! {
-    use alloc::format;
-    let msg = if let Some(location) = info.location() {
-        format!(
-            "Panic!\n\n{}\n\n[{} : Ln {}, Col {}]\0",
-            info.message(),
-            location
-                .file()
-                .strip_prefix("/home/gerald/Projects/Rust/rust-wiiu/")
-                .unwrap_or(location.file()),
-            location.line(),
-            location.column()
-        )
-    } else {
-        format!("Panic!\n\n{}\0", info.message())
-    };
-
-    unsafe {
-        crate::bindings::OSFatal(msg.as_ptr() as *const _);
-    }
-    loop {}
-}
